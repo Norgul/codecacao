538a788e32337c3cb45f814846dbc7bf8ca18516
Marko Dupor <marko.dupor@gmail.com>
2017-08-05 13:20:35 +0200

    extracted script to external file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,49 +37,5 @@
 
 </div>
 
-
-<script>
-
-    var uri = $(location).prop('href').split('/')[3];
-
-    $(function () {
-        loadResource(uri);
-    });
-
-    $("#submit_rating").on('click', function () {
-        submitRating($('#visitor_id').val(), $('#rating').val())
-    });
-
-    function loadResource(uri) {
-        $.ajax({
-            url: "/api/uri/" + uri
-        }).done(function (data) {
-            if (!$.trim(data)) {
-                alert("No URI found by given name");
-                return;
-            }
-
-            $("#current_resource").html('').append("<h2>Resource: " + data.uri + "</h2><h2>Current rating: " + data.sum_ratings / data.sum_users + "</h2>")
-        });
-    }
-
-    function submitRating(visitor_id, rating) {
-        $.ajax({
-            type: 'POST',
-            url: "/api/rating",
-            data: {
-                "visitor_id": visitor_id,
-                "rating": rating,
-                "uri": uri
-            }
-        }).done(function (data) {
-            loadResource(uri);
-        }).fail(function (error){
-            alert('Submission failed: ' + JSON.stringify(error));
-        });
-    }
-</script>
-
-
 </body>
 </html>
